e11060ea0774937c85acc45fe8ae70d6efd28044
Ryxali <ryxali@gmail.com>
2015-06-23 09:58:03 +0200

    Pre work for spawnings

---
+++ Source/Gauntlet/SpawnPointComponent.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gauntlet.h"
+#include "SpawnPointComponent.h"
+#include "ParticleDefinitions.h"
+#include "Engine.h"
+#include "math.h"
+
+// Sets default values for this component's properties
+USpawnPointComponent::USpawnPointComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+	// ...
+	struct FConstructorStatics
+	{
+		// A helper class object we use to find target UTexture2D object in resource package
+		ConstructorHelpers::FObjectFinderOptional<UTexture2D> MarkerTextureObject;
+		// Icon sprite category name
+		FName ID_CatagoryName;
+			// Icon sprite display name
+		FText NAME_DisplayName;
+		FConstructorStatics()
+			// Use helper class object to find the texture
+			// "/Engine/EditorResources/S_Note" is resource path
+			: MarkerTextureObject(TEXT("/Engine/EditorResources/S_Actor"))
+			, ID_CatagoryName(TEXT("Marker"))
+			, NAME_DisplayName(NSLOCTEXT("SpriteCategory", "AmmoMarker", "AmmoMarker"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+	BillboardComponent = ObjectInitializer.CreateEditorOnlyDefaultSubobject<UBillboardComponent>(this, TEXT("Billboard"), true);
+	SpriteTexture = ConstructorStatics.MarkerTextureObject.Get();
+	
+	
+	BillboardComponent->Sprite = SpriteTexture;
+
+	BillboardComponent->AttachTo(this);
+}
+
+
+// Called when the game starts
+void USpawnPointComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USpawnPointComponent::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
+void USpawnPointComponent::OnRegister()
+{
+	// If we need to perform a call to AttachTo, do that now
+	// At this point scene component still has no any state (rendering, physics),
+	// so this call will just add this component to an AttachChildren array of a the Parent component
+	AttachTo(AttachParent, AttachSocketName);
+	Super::OnRegister();
+}
+
+void USpawnPointComponent::Spawn()
+{
+	FMath::RandRange(0, Types.Num()-1);
+	UWorld::SpawnActor<Types[0]>();
+}
---
+++ Source/Gauntlet/SpawnPointComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/SceneComponent.h"
+#include "SpawnPointComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAUNTLET_API USpawnPointComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	virtual void OnRegister() override;
+	// Sets default values for this component's properties
+	USpawnPointComponent(const FObjectInitializer &ObjectInitializer);
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+	//UPROPERTY(EditDefaultsOnly, Category = Sprite)
+	//class UTexture2D* Sprite;
+	/** The different types to choose from when spawning */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Types")
+	TArray<TSubclassOf<class AActor>> Types;
+
+	UFUNCTION(BlueprintCallable, Category = "Spawn")
+	void Spawn();
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Types")
+	
+protected:
+	//A UBillboardComponent to hold Icon sprite
+	class UBillboardComponent* BillboardComponent;
+	//Sprite for the Billboard Component
+	class UTexture2D* SpriteTexture;
+private:
+		
+	
+};
